0e76669096bdc64a02634b70bc9d9c91cffaf19c
Yogitha-20 <47441174+Yogitha-20@users.noreply.github.com>
2022-05-18 22:52:00 +0530

    compute_stats and alerts added

--- stats.cpp
+++ stats.cpp
@@ -2,4 +2,55 @@
 
 Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
     //Implement statistics here
+   float average;
+   float min = vec[0];
+   float max = vec[0];
+   auto const count = static_cast<float>(vec.size());
+   if(vec.empty())
+        {std::cout<<"It's NaN"<<"\n"}
+
+    for (auto i=0 ; i<vec.size() ; i++)
+    {
+        float sum=0;
+        sum=sum+i;
+        average = sum/count;
+        if( max < vec[i])
+        {
+            max = vec[i];   
+        }
+        if( min > vec[i])
+        {
+            min = vec[i];  
+        }
+    }
+    
+}
+void EmailAlert::emailSent()
+{
+  cout<<"Email sent";
+}
+void LEDAlert::ledGlows()
+{
+  cout<<"LED glows";
+}
+void Statistics::StatsAlerter(const float maxthreshold,std::vector< > alerter)
+    {
+       float threshold=maxthreshold;
+    }
+void Statistics::StatsAlerter::checkandalert(vector<float> vec)
+{
+    float max = vec[0]
+      for (auto i=0 ; i<vec.size() ; i++)
+    {
+        
+        if( max < vec[i])
+        {
+            max = vec[i];   
+        }
+    }
+    if (max > threshold){
+        EmailAlert.emailsent()
+        LEDAlert.ledGlows()
+
+    }
 }
--- stats.h
+++ stats.h
@@ -1,5 +1,22 @@
 #include <vector>
 
-namespace Statistics {
+namespace Statistics 
+{
     Stats ComputeStatistics(const std::vector<___>& );
+
+    class EmailAlert
+    {
+        void emailSent();
+    };
+    class LEDAlert
+    {
+        void ledGlows();   
+    };
+    class StatsAlerter
+    {
+        StatsAlerter();
+        StatsAlerter(const float maxthreshold,std::vector< > alerter);
+        void checkandalert(vector<float> vec);
+    
+    };
 }
